32c7f03c3719ae3e0745a2ffacb9e9f3f5dd8f8a
Dai MIKURUBE <dmikurube@acm.org>
2015-12-05 20:31:08 +0000

    Migrate the db for dropping user_events.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205201013) do
+ActiveRecord::Schema.define(version: 20151205202322) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -29,13 +29,6 @@ ActiveRecord::Schema.define(version: 20151205201013) do
     t.datetime "updated_at",             null: false
     t.integer  "user_id",    default: 0, null: false
     t.string   "name"
-  end
-
-  create_table "user_events", force: :cascade do |t|
-    t.string   "user_id"
-    t.integer  "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
