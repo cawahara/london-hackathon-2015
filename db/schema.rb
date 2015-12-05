ad7e7821bcb37d50ddb0f18cf798590d42a075a2
Dai MIKURUBE <dmikurube@acm.org>
2015-12-05 20:39:23 +0000

    Migrate the db for the new table "attendance".
    $ rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205202322) do
+ActiveRecord::Schema.define(version: 20151205203809) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
